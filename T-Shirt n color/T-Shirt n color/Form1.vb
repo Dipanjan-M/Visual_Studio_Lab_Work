b880ea00ec00ea4f69c1aef234ad1ef4abe9572c
DIPANJAN MAITY <55407059+Dipanjan-M@users.noreply.github.com>
2020-11-30 15:04:54 +0530

    Add files via upload

---
+++ T-Shirt n color/T-Shirt n color/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "T-Shirt"
+        GroupBox1.Text = "Size"
+        GroupBox2.Text = "Color"
+        RadioButton1.Text = "Small"
+        RadioButton2.Text = "Medium"
+        RadioButton3.Text = "Large"
+        RadioButton4.Text = "Red"
+        RadioButton5.Text = "Green"
+        RadioButton6.Text = "Blue"
+        Button1.Text = "Price"
+        Button2.Text = "Exit"
+        Button3.Text = "Submit"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Not yet declared.")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim s1, s2 As String
+        s1 = ""
+        s2 = ""
+        If RadioButton1.Checked Then
+            s1 = RadioButton1.Text
+        ElseIf RadioButton2.Checked Then
+            s1 = RadioButton2.Text
+        ElseIf RadioButton3.Checked Then
+            s1 = RadioButton3.Text
+        End If
+        If RadioButton4.Checked Then
+            s2 = RadioButton4.Text
+        ElseIf RadioButton5.Checked Then
+            s2 = RadioButton5.Text
+        ElseIf RadioButton6.Checked Then
+            s2 = RadioButton6.Text
+        End If
+        If s1 = "" Then
+            MessageBox.Show("Selsect any size")
+        ElseIf s2 = "" Then
+            MessageBox.Show("Select any color")
+        Else
+            MsgBox("User selected a " & s2 & " color and " & s1 & " size T-shirt", , "User's choice")
+        End If
+    End Sub
+End Class
---
+++ T-Shirt n color/T-Shirt n color/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T-Shirt n color")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("T-Shirt n color")> 
+<Assembly: AssemblyCopyright("Copyright ©  2019")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c53850c6-982e-43b6-8271-1357f1d1dd90")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
